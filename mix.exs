0834fec90cc08bfad3971b935845a8d1cfc1de12
Atlas <atlas@octocon.app>
2026-01-19 15:15:52 -0500

    chore: Update dependencies

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Octocon.MixProject do
     [
       app: :octocon,
       version: "0.1.0",
-      elixir: "~> 1.17",
+      elixir: "~> 1.19",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
@@ -47,20 +47,20 @@ defmodule Octocon.MixProject do
       # Phoenix boilerplate
       {:phoenix, "~> 1.7.21"},
       {:phoenix_ecto, "~> 4.6"},
-      {:ecto_sql, "~> 3.12"},
+      {:ecto_sql, "~> 3.13"},
       {:ecto_psql_extras, "~> 0.8"},
-      {:postgrex, "~> 0.20"},
+      {:postgrex, "~> 0.21"},
       {:phoenix_live_dashboard, "~> 0.8"},
 
       # Scylla
       {:exandra, "~> 0.14"},
 
       # Use Bandit instead of Cowboy
-      {:bandit, "~> 1.6"},
+      {:bandit, "~> 1.10"},
       {:websock_adapter, "~> 0.5"},
 
       # Authentication
-      {:guardian, "~> 2.3"},
+      {:guardian, "~> 2.4"},
       {:guardian_db, "~> 3.0"},
       {:ueberauth, "~> 0.10"},
       {:ueberauth_google, "~> 0.10"},
@@ -75,20 +75,21 @@ defmodule Octocon.MixProject do
       # Discord
       # {:nostrum, github: "Kraigie/nostrum", branch: "master", override: true, runtime: false},
       {:nostrum, "~> 0.10", override: true, runtime: false},
-      {:certifi, "~> 2.13", override: true},
+      {:certifi, "~> 2.16", override: true},
       # Use zstd for gateway compression
-      {:ezstd, "~> 1.1"},
+      {:ezstd, "~> 1.2"},
       # {:nosedrum, "~> 0.6"},
       # {:nosedrum,
       # github: "jchristgit/nosedrum", branch: "master", override: true},
 
       # Caching
       {:cachex, "~> 4.1"},
+      {:trie_hard, "~> 0.2"},
       # Background jobs
       {:oban, "~> 2.19.4"},
       # Object storage
       {:waffle, "~> 1.1"},
-      {:image, "~> 0.59"},
+      {:image, "~> 0.62"},
       {:ex_aws, "~> 2.5"},
       {:ex_aws_s3, "~> 2.5"},
 
@@ -103,9 +104,7 @@ defmodule Octocon.MixProject do
       {:timex, "~> 3.7"},
 
       # Distribution
-      {:fly_postgres, "~> 0.3"},
       {:horde, "~> 0.9.0"},
-      {:dns_cluster, "~> 0.2"},
       {:libcluster, "~> 3.3"},
 
       # Time-series data
@@ -120,7 +119,7 @@ defmodule Octocon.MixProject do
       {:sweet_xml, "~> 0.7.4"},
       {:hackney, "~> 1.20"},
       {:jason, "~> 1.4"},
-      {:finch, "~> 0.19"},
+      {:finch, "~> 0.20"},
       {:nanoid, "~> 2.1.0"},
 
       # Dev-only
