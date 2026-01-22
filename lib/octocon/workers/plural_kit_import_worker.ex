ea94bba137e349783de2bd79b58ad4d7483ce94d
Atlas <atlas@octocon.app>
2026-01-22 03:43:44 -0500

    refactor: Use Scylla `BATCH` for PK/SP import atomicity (wip)

--- lib/octocon/workers/plural_kit_import_worker.ex
+++ lib/octocon/workers/plural_kit_import_worker.ex
@@ -198,6 +198,45 @@ defmodule Octocon.Workers.PluralKitImportWorker do
     }
   end
 
+  defp alter_to_insert_query(%Alter{
+    user_id: user_id,
+    id: id,
+    name: name,
+    proxy_name: proxy_name,
+    discord_proxies: discord_proxies,
+    pronouns: pronouns,
+    description: description,
+    alias: aliaz,
+    pinned: pinned,
+    archived: archived,
+    last_fronted: last_fronted,
+    color: color,
+    fields: fields,
+    inserted_at: inserted_at,
+    updated_at: updated_at
+  }, region) do
+    query = "INSERT INTO #{region}.alters (user_id, id, name, proxy_name, discord_proxies, pronouns, description, alias, pinned, archived, last_fronted, color, fields, inserted_at, updated_at) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"
+    values = [
+      user_id,
+      id,
+      name,
+      proxy_name,
+      discord_proxies,
+      pronouns,
+      description,
+      aliaz,
+      pinned,
+      archived,
+      last_fronted,
+      color,
+      fields,
+      inserted_at,
+      updated_at
+    ]
+
+    {query, values}
+  end
+
   defp default_if_empty(string, max, default \\ nil)
 
   defp default_if_empty(nil, _max, default), do: default
