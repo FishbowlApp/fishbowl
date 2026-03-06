da32ba3658f9e6c3be8fad945e74f397defe9dc4
Atlas <atlas@octocon.app>
2026-03-06 01:21:44 -0500

    fix: Ignore invalid field correlations in SP imports

--- lib/octocon/workers/simply_plural_import_worker.ex
+++ lib/octocon/workers/simply_plural_import_worker.ex
@@ -385,16 +385,17 @@ defmodule Octocon.Workers.SimplyPluralImportWorker do
         untracked: type == :custom_front,
         last_fronted: nil,
         fields:
-          Enum.map(alter["info"] || [], fn {field_id, value} ->
-            octocon_field_id = Map.get(field_associations, field_id)
+          alter["info"] ||
+            []
+            |> Enum.map(fn {field_id, value} ->
+              octocon_field_id = Map.get(field_associations, field_id)
 
-            value = %Octocon.Alters.Field{
-              id: octocon_field_id,
-              value: value
-            }
-
-            value
-          end),
+              %Octocon.Alters.Field{
+                id: octocon_field_id,
+                value: value
+              }
+            end)
+            |> Enum.filter(fn field -> field.id != nil end),
         security_level: 3,
         inserted_at: NaiveDateTime.utc_now(:second) |> naive_datetime_to_datetime(),
         updated_at: NaiveDateTime.utc_now(:second) |> naive_datetime_to_datetime()
